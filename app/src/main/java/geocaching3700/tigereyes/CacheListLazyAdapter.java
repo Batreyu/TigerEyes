a0e448cb48bb7803294fcc6015f1e3f29ba494cf
Batreyu <karaborn@gmail.com>
2014-10-02 21:26:52 -0500

    Added save current location feature to MyLocation. Clicking my location from the main menu will now correctly populate previously saved locations.

---
+++ app/src/main/java/geocaching3700/tigereyes/CacheListLazyAdapter.java
@@ -0,0 +1,80 @@
+package geocaching3700.tigereyes;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+/**
+ * Created by Kara on 10/2/2014.
+ */
+
+public class CacheListLazyAdapter extends ArrayAdapter<Cache> {
+private Activity activity;
+private int listitem;
+private ArrayList<Cache> caches;
+private static LayoutInflater inflater=null;
+//public ImageLoader imageLoader;
+
+    public CacheListLazyAdapter(Context context, int textViewResourceId, ArrayList<Cache> objects) {
+        super(context, textViewResourceId, objects);
+        caches = objects;
+    }
+
+public void LazyAdapter(Activity a,int rowID, ArrayList<Cache> cachelist) {
+        activity = a;
+        caches = cachelist;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+       // imageLoader=new ImageLoader(activity.getApplicationContext());
+        }
+
+public int getCount() {
+
+    return caches.size();
+        }
+
+//public int getItem(int position) {
+       // return position;
+        //}
+
+public long getItemId(int position) {
+        return position;
+        }
+
+public View getView(int position, View convertView, ViewGroup parent) {
+        View vi=convertView;
+
+    if (vi == null) {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        vi = inflater.inflate(R.layout.cache_item, null);
+    }
+
+
+        TextView Name = (TextView)vi.findViewById(R.id.text1); // title of cache
+        TextView Desc = (TextView)vi.findViewById(R.id.text2); // desc or location
+        ImageView thumb_image=(ImageView)vi.findViewById(R.id.icon); // image
+
+        Cache thisCache = caches.get(position);
+        Name.setText(thisCache.getTitle());
+        String coords = "Latitude: " +  thisCache.getLat() + "Longitude: " + thisCache.getLon();
+        Desc.setText(coords);
+        //ADD code to for JSON here
+        return vi;
+        }
+ }
+
+
+
+
+
+
+
--- app/src/main/java/geocaching3700/tigereyes/MyLocation.java
+++ app/src/main/java/geocaching3700/tigereyes/MyLocation.java
@@ -1,54 +1,141 @@
 package geocaching3700.tigereyes;
 
-//CURRENTLY NOT USED
-        import android.os.Bundle;
-        import android.app.Activity;
-        import android.content.Context;
-        import android.location.Location;
-        import android.location.LocationListener;
-        import android.location.LocationManager;
-        import android.widget.TextView;
 
-        import android.util.Log;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MyLocation extends Activity implements LocationListener{
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class MyLocation extends Activity {
     protected Context context;
-    TextView txtLat;
-    String lat;
-    String provider;
-    protected String latitude,longitude;
-    protected boolean gps_enabled,network_enabled;
+    private ListView listView;
+    private TextView saveLoc;
+    private LocationManager mgr = null;
+    private LocationListener listener = null;
+    private Location lastKnownLocation = null;
+    private SharedPreferences settings;
+    private  float longitude, latitude;
+    private DatabaseHandler dbHandler;
+    private Activity a;
+    Cache currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        txtLat = (TextView) findViewById(R.id.textview1);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
+        setContentView(R.layout.my_locations);
+        a = this;
+        saveLoc = (TextView) findViewById(R.id.topbar);
+        listView = (ListView) findViewById(R.id.cachelist);
+        mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new MyLocationListener();
+        context = getApplicationContext();
+        settings = getSharedPreferences("settings", MODE_WORLD_WRITEABLE);
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        dbHandler = new DatabaseHandler(context);
+        ArrayList<Cache> caches = dbHandler.getCaches();
+        final CacheListLazyAdapter adapter= new CacheListLazyAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                caches );
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-    }
-    @Override
-    public void onLocationChanged(Location location) {
-        txtLat = (TextView) findViewById(R.id.textview1);
-        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        listView.setAdapter(adapter);
+        //CacheListLazyAdapter adapter = new CacheListLazyAdapter(a, R.layout.cache_item, caches));
+
+        saveLoc.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //  v.setAnimation(AnimationUtils.loadAnimation(this, R.anim.image_click));
+
+                final Dialog d = new Dialog(MyLocation.this);
+                d.setContentView(R.layout.save_location);
+                d.setTitle("Please Name This Location");
+                d.show();
+                final EditText name = (EditText) d.findViewById(R.id.edittext);
+                Button ok = (Button) d.findViewById(R.id.dialogButtonOK);
+                Button cancel = (Button) d.findViewById(R.id.dialogButtonCancel);
+
+                cancel.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        d.dismiss();
+                    }
+                });
+                ok.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                     String locName = name.getText().toString();
+                     float lat = settings.getFloat("Latitude", 0);
+                     float lon =  settings.getFloat("Longitude", 0);
+                        currentLocation = new Cache(locName,lat,lon);
+                        dbHandler.addCache(currentLocation);
+                        CharSequence text = "Location Saved";
+                        int duration = Toast.LENGTH_LONG;
+                        adapter.notifyDataSetChanged();
+                        d.dismiss();
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
+                // currentLocation = new Cache();
+
+
+
+            }
+        });
     }
 
-    @Override
-    public void onProviderDisabled(String provider) {
-        Log.d("Latitude","disable");
+
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(final Location location) {
+            lastKnownLocation = location;
+            longitude = (float) location.getLongitude();
+            latitude = (float) location.getLatitude();
+            settings = getSharedPreferences("settings", MODE_WORLD_WRITEABLE);
+            SharedPreferences.Editor prefEditor = settings.edit();
+            prefEditor.putFloat("Latitude", (float) location.getLatitude());
+            prefEditor.putFloat("Longitude", (float)location.getLongitude());
+            prefEditor.commit();
+        }
+
+        @Override
+        public void onProviderDisabled(final String provider) {
+        }
+
+        @Override
+        public void onProviderEnabled(final String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(final String provider, final int status, final Bundle extras) {
+        }
     }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-        Log.d("Latitude","enable");
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude","status");
-    }
-
+    ;
 }
+
+
+
