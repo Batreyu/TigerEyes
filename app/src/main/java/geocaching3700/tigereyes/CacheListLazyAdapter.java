d1b6b15a5789ac34a17e6fbba4f38bed14da5308
Batreyu <karaborn@gmail.com>
2014-10-06 20:59:43 -0500

    Added destination coordinates to Start Navigation. List of caches are now clickable and show details. Clicking go launches start navigation. Fixed layout. Added new launch icon

--- app/src/main/java/geocaching3700/tigereyes/CacheListLazyAdapter.java
+++ app/src/main/java/geocaching3700/tigereyes/CacheListLazyAdapter.java
@@ -1,6 +1,4 @@
 package geocaching3700.tigereyes;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import android.app.Activity;
 import android.content.Context;
@@ -8,10 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
 
 /**
@@ -19,10 +17,10 @@ import android.widget.TextView;
  */
 
 public class CacheListLazyAdapter extends ArrayAdapter<Cache> {
-private Activity activity;
-private int listitem;
-private ArrayList<Cache> caches;
-private static LayoutInflater inflater=null;
+    private static LayoutInflater inflater = null;
+    private Activity activity;
+    private int listitem;
+    private ArrayList<Cache> caches;
 //public ImageLoader imageLoader;
 
     public CacheListLazyAdapter(Context context, int textViewResourceId, ArrayList<Cache> objects) {
@@ -30,47 +28,47 @@ private static LayoutInflater inflater=null;
         caches = objects;
     }
 
-public void LazyAdapter(Activity a,int rowID, ArrayList<Cache> cachelist) {
+    public void LazyAdapter(Activity a, int rowID, ArrayList<Cache> cachelist) {
         activity = a;
         caches = cachelist;
-        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-       // imageLoader=new ImageLoader(activity.getApplicationContext());
-        }
-
-public int getCount() {
-
-    return caches.size();
-        }
-
-//public int getItem(int position) {
-       // return position;
-        //}
-
-public long getItemId(int position) {
-        return position;
-        }
-
-public View getView(int position, View convertView, ViewGroup parent) {
-        View vi=convertView;
-
-    if (vi == null) {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        vi = inflater.inflate(R.layout.cache_item, null);
+        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // imageLoader=new ImageLoader(activity.getApplicationContext());
     }
 
+    public int getCount() {
 
-        TextView Name = (TextView)vi.findViewById(R.id.text1); // title of cache
-        TextView Desc = (TextView)vi.findViewById(R.id.text2); // desc or location
-        ImageView thumb_image=(ImageView)vi.findViewById(R.id.icon); // image
+        return caches.size();
+    }
+
+//public int getItem(int position) {
+    // return position;
+    //}
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+
+        if (vi == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            vi = inflater.inflate(R.layout.cache_item, null);
+        }
+
+
+        TextView Name = (TextView) vi.findViewById(R.id.text1); // title of cache
+        TextView Desc = (TextView) vi.findViewById(R.id.text2); // desc or location
+        ImageView thumb_image = (ImageView) vi.findViewById(R.id.icon); // image
 
         Cache thisCache = caches.get(position);
         Name.setText(thisCache.getTitle());
-        String coords = "Latitude: " +  thisCache.getLat() + "Longitude: " + thisCache.getLon();
+        String coords = "Latitude: " + thisCache.getLat() + ", Longitude: " + thisCache.getLon();
         Desc.setText(coords);
         //ADD code to for JSON here
         return vi;
-        }
- }
+    }
+}
 
 
 
--- app/src/main/java/geocaching3700/tigereyes/MyLocation.java
+++ app/src/main/java/geocaching3700/tigereyes/MyLocation.java
@@ -1,32 +1,30 @@
 package geocaching3700.tigereyes;
 
 
+import android.app.Activity;
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-
 import java.util.ArrayList;
 
 public class MyLocation extends Activity {
     protected Context context;
+    Cache currentLocation;
     private ListView listView;
     private TextView saveLoc;
     private LocationManager mgr = null;
@@ -36,7 +34,6 @@ public class MyLocation extends Activity {
     private  float longitude, latitude;
     private DatabaseHandler dbHandler;
     private Activity a;
-    Cache currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +42,7 @@ public class MyLocation extends Activity {
         getActionBar().hide();
         setContentView(R.layout.my_locations);
         a = this;
-        saveLoc = (TextView) findViewById(R.id.topbar);
+        saveLoc = (TextView) findViewById(R.id.saveloc);
         listView = (ListView) findViewById(R.id.cachelist);
         mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
         listener = new MyLocationListener();
@@ -61,7 +58,49 @@ public class MyLocation extends Activity {
 
         listView.setAdapter(adapter);
         //CacheListLazyAdapter adapter = new CacheListLazyAdapter(a, R.layout.cache_item, caches));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final Dialog d = new Dialog(MyLocation.this);
+                d.setContentView(R.layout.cache_details);
+                d.setTitle("Details");
+                d.show();
+                final Cache thisCache = (Cache) listView.getItemAtPosition(position);
+                TextView title = (TextView) d.findViewById(R.id.title);
+                TextView coords = (TextView) d.findViewById(R.id.coords);
+                TextView distance = (TextView) d.findViewById(R.id.distance);
+                TextView direction = (TextView) d.findViewById(R.id.direction);
+                Button go = (Button) d.findViewById(R.id.dialogButtonOK);
+                Button cancel = (Button) d.findViewById(R.id.dialogButtonCancel);
+                title.setText(thisCache.getTitle());
+                coords.setText("Latitude: " + thisCache.getLat() + ", Longitude: " + thisCache.getLon());
+                //use shared prefs to calculate distance and direction
 
+                cancel.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        d.dismiss();
+                    }
+                });
+                go.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        float destLat = thisCache.getLat();
+                        float destLon = thisCache.getLon();
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putFloat("destLatitude", destLat);
+                        editor.putFloat("destLongitude", destLon);
+                        editor.putString("destTitle", thisCache.getTitle());
+                        editor.commit();
+                        d.dismiss();
+                        //launch start nav
+                        Intent intent = new Intent(MyLocation.this, StartNavigation.class);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+        });
         saveLoc.setOnClickListener(new OnClickListener() {
 
             @Override
@@ -70,7 +109,7 @@ public class MyLocation extends Activity {
 
                 final Dialog d = new Dialog(MyLocation.this);
                 d.setContentView(R.layout.save_location);
-                d.setTitle("Please Name This Location");
+                d.setTitle("Name This Location");
                 d.show();
                 final EditText name = (EditText) d.findViewById(R.id.edittext);
                 Button ok = (Button) d.findViewById(R.id.dialogButtonOK);
@@ -86,8 +125,8 @@ public class MyLocation extends Activity {
                     @Override
                     public void onClick(View v) {
                      String locName = name.getText().toString();
-                     float lat = settings.getFloat("Latitude", 0);
-                     float lon =  settings.getFloat("Longitude", 0);
+                        float lat = settings.getFloat("currentLatitude", 0);
+                        float lon = settings.getFloat("currentLongitude", 0);
                         currentLocation = new Cache(locName,lat,lon);
                         dbHandler.addCache(currentLocation);
                         CharSequence text = "Location Saved";
